44c92ccffb5a8f2caf60b727f4bedd35b9ef5f34
amilisa <iamelizabethks@gmail.com>
2021-04-09 13:36:05 +0700

    Implement Dijkstra's Algorithm

--- path_finding_algorithm.py
+++ path_finding_algorithm.py
@@ -1,4 +1,6 @@
 import pygame
+import heapdict
+
 from constants.sizes import CELL_SIZE
 from constants.colors import *
 
@@ -13,37 +15,60 @@ def trace_path(root_node, goal_node):
     return path
 
 
-def visualise_steps(current_node, root_node, goal_node, array, grid_wrapper):
+def visualise_steps(current_node, root_node, goal_node, nodes_list, grid_wrapper):
     if current_node is not root_node:
         current_node.draw_cell(grid_wrapper, CELL_SIZE, LIGHT_GREY)
 
-    for node in array:
+    for node in nodes_list:
         if node is not goal_node:
             node.draw_cell(grid_wrapper, CELL_SIZE, LIGHT_YELLOW)
+    pygame.time.delay(5)
     pygame.display.update()
 
 
 def breadth_first_search(root_node, goal_node, grid, grid_wrapper):
     queue = [root_node]
-    root_node.is_observed = True
+    root_node.is_visited = True
     while queue:
         current_node = queue.pop(0)
         if current_node is goal_node:
             path = trace_path(root_node, goal_node)
             return path
 
-        current_node.add_neighbors(grid)
-        for neighbor in current_node.neighbors:
-            if not neighbor.is_observed:
-                neighbor.is_observed = True
-                queue.append(neighbor)
-                neighbor.previous = current_node
+        current_node.add_unvisited_neighbors(grid)
+        neighbors_list = current_node.neighbors
+        visualise_steps(current_node, root_node, goal_node, neighbors_list, grid_wrapper)
 
-        visualise_steps(current_node, root_node, goal_node, queue, grid_wrapper)
+        for neighbor in neighbors_list:
+            queue.append(neighbor)
+            neighbor.is_visited = True
+            neighbor.previous = current_node
 
     return []
 
 
+def dijkstra(root_node, goal_node, grid, grid_wrapper):
+    root_node.distance = 0
+    root_node.is_visited = True
+    priority_queue = heapdict.heapdict()
+    priority_queue[root_node] = root_node.distance
 
+    while priority_queue:
+        current_node = priority_queue.popitem()[0]
+        current_node.is_visited = True
+        if current_node is goal_node:
+            path = trace_path(root_node, goal_node)
+            return path
 
+        current_node.add_unvisited_neighbors(grid)
+        neighbors_list = current_node.neighbors
+        visualise_steps(current_node, root_node, goal_node, neighbors_list, grid_wrapper)
 
+        for neighbor in neighbors_list:
+            new_distance = current_node.distance + neighbor.cost
+            if new_distance < neighbor.distance:
+                neighbor.distance = new_distance
+                priority_queue[neighbor] = new_distance
+                neighbor.previous = current_node
+
+    return []
